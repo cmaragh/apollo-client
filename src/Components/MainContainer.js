c042477b5a48e730dd518f364ba835fbd047d6b7
cmaragh <53223685+cmaragh@users.noreply.github.com>
2020-11-27 17:51:41 -0500

    Added theming to container

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,9 +1,18 @@
 import React from "react";
 import TextContainer from "./TextContainer";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    padding: "1em 2.5em"
+  }
+});
 
 const MainContainer = (props) => {
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       <img
         src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F56993b52-55ed-45c4-8db7-d1c6a3f62cbc%2Fperson.png?table=block&id=f5036f0b-854a-4360-b84d-77da3872d0d3&width=730&userId=&cache=v2"
         alt=""
--- src/Components/TextContainer.js
+++ src/Components/TextContainer.js
@@ -1,26 +1,54 @@
 import React from "react";
 import { Button } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider
+} from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   root: {
     display: "flex",
     flexDirection: "column"
+  },
+  btn: {
+    backgroundColor: "#3C90C9"
+  }
+});
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#3C90C9",
+      dark: "#002884",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000"
+    }
   }
 });
 
 const TextContainer = (props) => {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <h1>Your Brand on Your Links</h1>
-      <h3>
-        Rebrandly is the industry-leading link management platform to brand,
-        track and share short URLs using a custom domain name
-      </h3>
-      <Button>Sign up free</Button>
-      <Button>Request a demo</Button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <h1>Your Brand on Your Links</h1>
+        <h3>
+          Rebrandly is the industry-leading link management platform to brand,
+          track and share short URLs using a custom domain name
+        </h3>
+        <Button size="small" color="primary" variant="contained">
+          Sign up free
+        </Button>
+        <Button>Request a demo</Button>
+      </div>
+    </ThemeProvider>
   );
 };
 
