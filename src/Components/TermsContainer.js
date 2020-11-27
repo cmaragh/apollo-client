9daefe6d495c70ec185a22bca76a093f3b662931
cmaragh <53223685+cmaragh@users.noreply.github.com>
2020-11-27 18:22:39 -0500

    Adjusted Terms Container styling

--- src/Components/TermsContainer.js
+++ src/Components/TermsContainer.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import "./TermsContainer.css";
+
 const TermsContainer = (props) => {
   return (
-    <div style={{ textAlign: "left" }}>
+    <div className="terms-container">
       <p>Terms and Conditions</p>
       <p>Privacy Policy</p>
       <p>Cookie Policy</p>
